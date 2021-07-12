cfabf827fc40f4e464e2609a753a44ad2d0ad930
NKlIl <87281513+NKlIl@users.noreply.github.com>
2021-07-13 01:25:06 +0300

    Update exper.py

--- exper.py
+++ exper.py
@@ -6,17 +6,22 @@ import streamlit as st
 
 def local_css(file_name):
     with open(file_name) as f:
-        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def remote_css(url):
-    st.markdown('<style src="{}"></style>'.format(url), unsafe_allow_html=True)
-
-def icon_css(icone_name):
-    remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
 
 def icon(icon_name):
-    st.markdown('<i class="material-icons">{}</i>'.format(icon_name), unsafe_allow_html=True)      
-      
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+icon("search")
+selected = st.text_input("", "Search...")
+button_clicked = st.button("OK")
+
+
 st.title('Страновед')
 st.subheader('Этот сайт знает столицу любой страны.')
 
