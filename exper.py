b4b0d8ac3e1385e8a6304f827115eaf8615382f4
NKlIl <87281513+NKlIl@users.noreply.github.com>
2021-07-13 01:13:58 +0300

    Update exper.py

--- exper.py
+++ exper.py
@@ -4,28 +4,30 @@ import math
 import pandas as pd
 import streamlit as st
 
-body {color: #fff; 
-      background-color: #4f8bf9;}
 
-div.element-container:nth-child(2) button {
-  color: blue;
-  border-radius: 50%;
-  height: 3em;
-  width: 3em;
-}
+import streamlit as st
+from PIL import Image
+import time
 
-div.element-container:nth-child(3) button {
-  color: red;
-  border-radius: 50%;
-  height: 3em;
-  width: 3em;
-}
-
-div.element-container:nth-child(4) button {
-  color: grey;
-  border-radius: 50%;
-  height: 3em;
-  width: 3em;
+placeholder = st.empty()
+st.header("Warning")
+for i in range(1, 1000):
+    if (i % 2) == 0:
+        
+        #caching.clear_cache()
+        placeholder = st.empty()
+        #Image.empty()
+        image = Image.open("green.jpg")
+        st.image(image, use_column_width=True, output_format="jpg")
+        #print("{0} is Even number". format(num))
+    else:
+        #caching.clear_cache()
+        placeholder = st.empty()
+        #Image.empty()
+        image = Image.open("red.jpg")
+        st.image(image, use_column_width=True, output_format="jpg")
+        #print("{0} is Odd number". format(num))
+    time.sleep(10)
 
 st.title('Страновед')
 st.subheader('Этот сайт знает столицу любой страны.')
