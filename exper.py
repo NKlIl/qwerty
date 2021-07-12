4289cb344a89c5f2d0be864a26ba840e2a8a46e1
NKlIl <87281513+NKlIl@users.noreply.github.com>
2021-07-13 01:21:09 +0300

    Update exper.py

--- exper.py
+++ exper.py
@@ -4,9 +4,39 @@ import math
 import pandas as pd
 import streamlit as st
 
+def main():
+    """
+    Heart of the streamlit App
+    """
+    # Some Basic Configuration for StreamLit
+    st.set_page_config(
+        page_title="Your Awesome App Title",
+        page_icon="path_of_your_favicon",
+        layout="wide",
+        initial_sidebar_state="auto",
+    )
+    pages = ["Home", "Page 1", "Page 2"]
+    p_choice = st.sidebar_selectbox("Menu", pages)
+    if p_choice == "Home":
+        local_css("style_1.css") # Relative path of the css file
+        st.title("Some Relevant Info")
+         .
+         .
+         .
+    else if p_choice == "Page 2":
+         local_css("style_2.css")
+         st.title("Title of Page 2")
+          .
+          .
+          .
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
-st.header("Warning")
+if __name__ ==  "__main__":
+     main()
+      
+      
 st.title('Страновед')
 st.subheader('Этот сайт знает столицу любой страны.')
 
