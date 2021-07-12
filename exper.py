2558f1f01472262314d27799526de17b1a8357e4
NKlIl <87281513+NKlIl@users.noreply.github.com>
2021-07-13 01:03:19 +0300

    Update exper.py

--- exper.py
+++ exper.py
@@ -4,24 +4,14 @@ import math
 import pandas as pd
 import streamlit as st
 
-[theme]
-
-# Primary accent for interactive elements
-primaryColor = '#7792E3'
-
-# Background color for the main content area
-backgroundColor = '#273346'
-
-# Background color for sidebar and most interactive widgets
-secondaryBackgroundColor = '#B9F1C0'
-
-# Color used for almost all text
-textColor = '#FFFFFF'
-
-# Font family for all text in the app, except code blocks
-# Accepted values (serif | sans serif | monospace) 
-# Default: "sans serif"
-font = "sans serif"
+st.markdown("""
+<style>
+body {
+    color: #fff;
+    background-color: #111;
+}
+</style>
+    """, unsafe_allow_html=True)
 
 st.title('Страновед')
 st.subheader('Этот сайт знает столицу любой страны.')
