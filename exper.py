141a4c8c8989bb6eda682021cc827a1ec567bf2e
NKlIl <87281513+NKlIl@users.noreply.github.com>
2021-07-12 22:13:08 +0300

    Update exper.py

--- exper.py
+++ exper.py
@@ -25,19 +25,3 @@ elif balloons == "🇷🇺 ":
       st.success("Столица России, Москва 🇷🇺.")
 
 
-elif balloons == '🇺🇸':
-      st.success('Столица США, Вашингтон 🇺🇸.')   
-elif balloons == 'сша':
-      st.success('Столица США, Вашингтон 🇺🇸.')
-elif balloons == 'соединённые штаты америки':
-      st.success('Столица Соединённых Штатов Америки, Вашингтон 🇺🇸.')
-elif balloons == 'United States of America':
-      st.success('Capital of the United States of America, Washington 🇺🇸.')    
-elif balloons == '🇳🇦':
-      st.success('Столица Намибии Виндхук 🇳🇦.')
-elif balloons == 'намибия':
-      st.success('Столица Намибии Виндхук 🇳🇦.')
-
-
-
-      
