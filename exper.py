47482b1b6e855182814092a3123c8f7ac580e3a1
NKlIl <87281513+NKlIl@users.noreply.github.com>
2021-07-13 01:14:43 +0300

    Update exper.py

--- exper.py
+++ exper.py
@@ -5,30 +5,8 @@ import pandas as pd
 import streamlit as st
 
 
-import streamlit as st
-from PIL import Image
-import time
 
-placeholder = st.empty()
 st.header("Warning")
-for i in range(1, 1000):
-    if (i % 2) == 0:
-        
-        #caching.clear_cache()
-        placeholder = st.empty()
-        #Image.empty()
-        image = Image.open("green.jpg")
-        st.image(image, use_column_width=True, output_format="jpg")
-        #print("{0} is Even number". format(num))
-    else:
-        #caching.clear_cache()
-        placeholder = st.empty()
-        #Image.empty()
-        image = Image.open("red.jpg")
-        st.image(image, use_column_width=True, output_format="jpg")
-        #print("{0} is Odd number". format(num))
-    time.sleep(10)
-
 st.title('Страновед')
 st.subheader('Этот сайт знает столицу любой страны.')
 
